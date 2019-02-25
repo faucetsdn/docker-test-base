97a953f1cbc1cc6b13da5dc981aef0054654dfa7
Brad Cowie <brad@wand.net.nz>
2019-02-25 16:55:37 +1300

    Tidy up test-base image.
      * Upgrade to Ubuntu 18.04.
      * Use virtualenv to contain python dependencies.
      * Use mk-build-deps to install build dependencies.
      * Remove unneeded dependencies.

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+python3 -m venv /venv
+source /venv/bin/activate
+
+git config --global url.https://github.com/.insteadOf git://github.com/
+
+mkdir -p $BUILDDIR
+
+cd $BUILDDIR
+wget -q -O- http://fast.dpdk.org/rel/dpdk-$DPDK.tar.xz | tar -Jxf-
+cd dpdk*
+make install T=$DPDK_TARGET DESTDIR=install
+
+cd $BUILDDIR
+git clone https://github.com/openvswitch/ovs -b ${OVSV}
+cd ovs
+./boot.sh
+./configure --enable-silent-rules --quiet --with-dpdk=`echo ../dpdk*/$DPDK_TARGET`
+make install
+
+cd $BUILDDIR
+git clone https://github.com/mininet/mininet
+cd mininet
+git checkout -b mininet-$MININETV $MININETV
+sed -i -e "s/setup.py install/${SETUPQ}/g" Makefile
+sed -i -e "s/apt-get/${AG}/g" util/install.sh
+for i in ssh pep8 pyflakes python-pexpect pylint xterm ; do
+    sed -i -e "s/${i}//g" util/install.sh
+done
+PYTHON=/venv/bin/python3 util/install.sh -n
