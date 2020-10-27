7f88646cb5179e87705eae6602f33f5bbe580e2a
Brad Cowie <brad@wand.net.nz>
2020-10-27 16:13:50 +1300

    Use ubuntu package for docker on i386.

---
+++ dind.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+arch=$(dpkg --print-architecture)
+
+case "${arch}" in
+    amd64)
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        echo "deb [arch=${arch}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+        ${AG} update
+        ${AG} install docker-ce
+    ;;
+    i386)
+        ${AG} install docker.io
+    ;;
+esac
