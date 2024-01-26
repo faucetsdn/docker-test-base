c629ed352a08cd5caa35740cd777ef36db1846c8
Brad Cowie <brad@faucet.nz>
2024-01-26 14:09:02 +1300

    Use custom docker init script on amd64 to fix issue setting ulimits.

--- bin/dind.sh
+++ bin/dind.sh
@@ -12,6 +12,7 @@ case "${arch}" in
         echo "deb [arch=${arch} signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${ID} ${VERSION_CODENAME} stable" > /etc/apt/sources.list.d/docker.list
         ${AG} update
         ${AG} install docker-ce
+        mv "${BUILD_DIR}/docker.init.d" /etc/init.d/docker
     ;;
     i386)
         ${AG} install docker.io
